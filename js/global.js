4943967470cd013e030ef47f250f3593f5c06724
Young Mi Kim <ymkim23@gmail.com>
2016-11-04 09:42:40 -0400

    update page layout of mainchirp and start global.js

--- js/global.js
+++ js/global.js
@@ -1,1 +1,18 @@
 //Local API host to use https://82e2e06a.ngrok.io/
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import PhotoUploader from '../components/'
+import PhotoDisplayer from '../components/'
+
+window.renderView = function() {
+  ReactDOM.render(
+    <div>
+      </>
+      </>
+    </div>,
+    document.querySelector('#app')
+  )
+}
+
+renderView()
